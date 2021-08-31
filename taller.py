93584cdefde3ad2d62ddf153afe6f816c6ef57c2
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-08-30 21:43:22 -0500

    Primer punto

--- taller.py
+++ taller.py
@@ -2,6 +2,11 @@
 """
 Created on Mon Aug 30 21:13:45 2021
 
-@author: Usuario
+@author: Laura Ojeda Méndez
 """
 
+# 1. y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+
+# Declaramos 'y' e ingresamos los valores a operar 
+y = ((5+2-5)**2 * 5+8/2 - 30) / 2*5-3
+print(f"EL valor de Y en el 1er punto es de: {y}")
