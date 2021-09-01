c471325ba036b247ea357c5d8a931a44280deb45
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-08-31 21:01:33 -0500

    Segundo punto

--- taller.py
+++ taller.py
@@ -7,6 +7,18 @@ Created on Mon Aug 30 21:13:45 2021
 
 # 1. y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
 
-# Declaramos 'y' e ingresamos los valores a operar 
+# Declaramos 'y' e ingresamos los operandos y valores a operar
 y = ((5+2-5)**2 * 5+8/2 - 30) / 2*5-3
 print(f"EL valor de Y en el 1er punto es de: {y}")
+
+# 2. z=5, n=3, m= z-n
+# y = (( (z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3
+
+# Declaramos las variables
+z = 5
+n = 3
+m = z - n
+
+# Declaramos 'y' e ingresamos los operandos y valores a operar
+y = (((z+2-n)**2 * m+8/2-30) / 2*5-3)**5 + 15*3 - 9/3
+print(f"EL valor de Y en el 2do punto es de: {y}")
