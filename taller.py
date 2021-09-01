7a793504f61d133d72a4fe445058f6c29bdcb53b
lauraOjeda98 <70666704+lauraOjeda98@users.noreply.github.com>
2021-08-31 21:20:52 -0500

    Tercer punto

--- taller.py
+++ taller.py
@@ -22,3 +22,13 @@ m = z - n
 # Declaramos 'y' e ingresamos los operandos y valores a operar
 y = (((z+2-n)**2 * m+8/2-30) / 2*5-3)**5 + 15*3 - 9/3
 print(f"EL valor de Y en el 2do punto es de: {y}")
+
+# 3. z=5, n=( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3, m= z^2*3+n
+# y = ((( (z+2-n)^2 x m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3) ^ 2 – 5/4
+
+# Declaramos las variables, realizamos las operaciones con estas e imprimimos Y
+z = 5
+n = ((8+2-4)**2 * 5+8+7/2 - 30*5) / 2*5-3
+m = z**2 * 3+n
+y = ((((z+2-n)**2 * m+8/2-30) / 2*5-3)**5 + 15*3-9/3)**2 - 5/4
+print(f"EL valor de Y en el 3er punto es de: {y}")
